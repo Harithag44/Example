db516b0649c0d442345e9732c89f1ead923e23b1
Harithag44 <150143615+Harithag44@users.noreply.github.com>
2024-06-27 10:18:42 +0530

    Update Helloworld.java
    hi

--- FirstPrj/src/com/vamritech/testcases/Helloworld.java
+++ FirstPrj/src/com/vamritech/testcases/Helloworld.java
@@ -8,20 +8,10 @@ public class Helloworld {
 		
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		var email = getSaltString() + "@gmail.com";
 		System.out.println(email);
 	}
 
-	protected static String getSaltString() {
-        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        StringBuilder salt = new StringBuilder();
-        Random rnd = new Random();
-        while (salt.length() < 6) { // length of the random string.
-            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
-            salt.append(SALTCHARS.charAt(index));
-        }
-        String saltStr = salt.toString();
-        return saltStr;
+	
 
-    }
 }
+
